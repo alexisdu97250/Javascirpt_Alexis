4797ec89c4cd683560df90182f1ca108a669fcf8
alexisdu97250 <baselalexis5@gmail.com>
2021-04-14 14:47:28 +0200

    Calcul salaire
    Probleme sur l'affichage des champs du formulaire

--- script_Alexis.js
+++ script_Alexis.js
@@ -1,193 +1,48 @@
-/*var maVar ="ma valeur";
-(function(){
-    var maVar = "Valeur locale";
-    console.log(maVar);
-})();*/
-
-/*function Calcul(num1,num2){
-    if(num2!=0){
-        return num1/num2;
-    }else{
-        console.log("Erreur: division par 0!");
-    }
-}
-
-
-
-var numero1=3;
-var numero2=5;
-console.log(numero1 + "/" + numero2 + "=" + Calcul(numero1,numero2));*/
-
-/*(function(){
-    var soldes="Toyota";
-    function carTypes(nom){
-        return(nom==="Honda") ?
-        nom: "Desolé, nous ne vendons pas de "+ nom;
-    }
-    var voiture{
-        marque: "Toyota",
-        getVoiture: carTypes("Hondaa")
-    };
-    console.log(voiture.marque);
-    console.log(voiture.getVoiture); 
-})*/
-
-//EXO 1
-/*function carre(b) { 
-
-return b*b; 
-
-} 
-
-// Afficher la sortie 
-
-console.log(carre(2)); 
-
-console.log(carre(4)); 
-
-//EXO 2 
-function somme100(a,b){
-    return (a+b)<100;
-}
-var a=50;
-var b=2;
-console.log("La somme de"+ a +" et " + b + "est-elle inferieure a 100 ?" + somme100(a,b));
-var a=50;
-var b=62;
-console.log("La somme de"+ a +" et " + b + "est-elle inferieure a 100 ?" + somme100(a,b));*/
-
-//EXO 3
-/*function test(a,b){
-    return a===b;
-}
-var a=51;
-var b="51";
-console.log("Test d'egalite de " + a + " et " + b + test(a,b));
-
-var a=51;
-var b=51;
-console.log("Test d'egalite de " + a + " et " + b + test(a,b));
-
-//EXO 4
-
-
-function reverseTab(tab) { 
-
-  let res = []; 
-
-  for (let i = tab.length -1; i > -1; i--) { 
-
-    res.push(tab[i]); 
-
-  } 
-
-   
-
-  return res; 
-
-} 
-
-// Afficher la sortie 
-
-console.log(reverseTab([1, 2, 3])); 
-
-console.log(reverseTab([1, 1, 2, 2, 3]));
-
-//EXO5
-
-function last(tab){
-    return tab[tab.length-1];
-}
-
-var tab=[1,5,9,877,"PSG"];
-console.log("Dernier element de " + tab + "=" + last(tab));*/
-
-//EXO RANDOM CLIENT
-/*var Noms=[];
-function tirage(){
-    let index = Math.floor( Math.random(Noms) * (Noms.length) );
-    let m=Noms[index];
-    console.log("Client tiré au sort " + m);
-    return m;
-}
-
-function add_nom(){
-    var nom=prompt("Saisir un nom");
-    Noms.push(nom);
-}
-
-//console.log(Math.floor(2.6));
-
-//For in
-var tab=["Paris","Nantes", "Lyon"];
-for(let name of tab){
-    console.log(name);
-}*/
-
-
-
-/*function Calcul(num1,num2){
-    try{
-        return num1/num2;
-    }catch(e){
-        console.log('Une erreur s''est produite :' + e);
-    }
-}
-
-var num1=4,num2=0;
-
-Calcul(num1,num2);*/
-
-
-//Objets
-
-function Document(numEnreg, titre){
-    this.numEnreg=numEnreg;
-    this.titre=titre;
-}
-
-function Livre(numEnreg, titre, Auteur, nbrPages){
-    Document.call(this,numEnreg, titre);
-    this.Auteur=Auteur;
-    this.nbrPages=nbrPages;
-    this.NbPages = function NbPages(){
-        //let Pages=this.nbrPages;
-        //console.log(Pages);
-        switch(this.nbrPages){
-            case (this.nbrPages <= 100) : 
-                console.log("Nombre de page entre 0 et 100");
-                break;
-            case (this.nbrPages <= 200) : 
-                console.log("Nombre de page entre 101 et 200");
-                break;
-            case (this.nbrPages <= 300) : 
-                console.log("Nombre de page entre 201 et 300");
-                break;
-            case (this.nbrPages > 300) : 
-                console.log("Nombre de page superieur a 301");
-                break;
-            //default :
-                //console.log("C'est bizarre quand meme");
+function Person(sexe, acharge,bonus, allocation,salairebrut){
+    this.sexe=sexe;
+    this.allocation=allocation;
+    this.acharge=acharge;
+    this.bonus=bonus;
+    this.salairebrut=salairebrut;
+    this.Salaire=function Salaire(){
+        let taxes=((0.18+0.07+0.05)*this.salairebrut);
+        if (this.sexe==="F"){
+            taxes-=0.02*salairebrut;
         }
+        
+        switch(this.acharge){
+            case 3:
+                taxes-=0.01*salairebrut;
+                break;
+            case 4:
+                taxes-=0.02*salairebrut;
+                break;
+            default:
+        }
+        let salaire=this.salairebrut-taxes;
+        if (this.bonus==="on"){
+            salaire+=100;
+        }
+        if (this.allocation==="on"){
+            salaire+=150;
+        }
+        return salaire;
     }
 }
 
-function Revue(numEnreg, titre, mois, annee){
-    Document.call(this,numEnreg, titre);
-    this.mois=mois;
-    this.annee=annee;
-}
-
-function Dictionnaire(numEnreg, titre, langue){
-    Document.call(this,numEnreg, titre);
-    this.langue=langue;
-}
+var grossSalary=document.getElementById("grossSalary").value;
+var additionBonus=document.getElementById("additionBonus").value;
+var gender=document.getElementById("gender").value;
+var additionAllowance=document.getElementById("additionAllowance").value;
+var dependents=document.getElementById("dependents").value;
 
 
 
-let doc=new Document(1,"Les fleurs du mal");
-Livre.prototype=Object.create(Document.prototype);
-let book = new Livre(2, "De La Guerre", "Clausewitz",200);
-console.log(book);
-book.NbPages();
+let person1=new Person(gender,dependents,additionBonus,additionAllowance,grossSalary);
+document.forms[0].elements["Salaire net"] = person1.Salaire();*/
+/*console.log(person1);
+console.log("Salaire net : "+person1.Salaire());*/
 
+/*let person2=new Person("Simon",27,"f",4,100, 45000);
+console.log(person2);
+console.log("Salaire net : "+person2.Salaire());
